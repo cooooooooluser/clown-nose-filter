206b2872c46a095d79e0a9906061eede7f085830
cooooooooluser <85533967+cooooooooluser@users.noreply.github.com>
2022-04-10 18:58:38 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
-noseX:0;
-noseY:0;
+noseX=0;
+noseY=0;
 
 function preload(){
-     
+    clown = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
-function setup () {
+function setup() {
     canvas= createCanvas(640, 480);
      canvas.position( 540, 250);
      video= createCapture(VIDEO);
     video.hide();
 
-
+     poseNet=ml5.poseNet(video, modelLoaded);
+     poseNet.on('pose', gotposes);
 }
 
-function draw () {
-    image(video, 0, 0, 640, 480)
+function draw() {
+    image(video, 0, 0, 640, 480);
+    image(clown, noseX, noseY , 50, 50);
+    
 }
 
-function savedPicture () {
-    //hi
+function modelLoaded() {
+      console.log("model has been loaded.");
+}
+
+function gotposes(results) {
+    console.log(results);
+    if (results.length > 0) {
+         noseX= results[0].pose.nose.x-25;
+         noseY= results[0].pose.nose.y-25;
+         console.log(noseX);
+         console.log(noseY);
+    }
+}
+
+function savedPicture() {
+    save("virus.png");
+
 }
